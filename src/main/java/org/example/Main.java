2d06d3427e57ab2c9499a5e4d65666aaa6f8bcc3
Zdesyatkov <Zdesyatkov1998@gmail.com>
2024-12-13 18:29:30 +0300

    add array

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,14 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        int[] array = {1, 2, 3, 4, 5, 6, 7};
+        for (int i = 0; i < 7; i++) {
+            if (array[i] == 5) {
+                break;
+            }
+            System.out.println(array[i]);
+        }
     }
+
 }
