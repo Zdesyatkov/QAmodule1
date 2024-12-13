7942ebb3ed4a57a5191df2e032eb011c45f90684
Zdesyatkov <Zdesyatkov1998@gmail.com>
2024-12-13 19:27:48 +0300

    add Practica1 and Practica2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,13 +3,4 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        int[] array = {1, 2, 3, 4, 5, 6, 7};
-        for (int i = 0; i < 7; i++) {
-            if (array[i] == 5) {
-                break;
-            }
-            System.out.println(array[i]);
-        }
-    }
-
-}
+}}
